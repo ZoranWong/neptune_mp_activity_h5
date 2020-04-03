ce045cc93822d20887a3ad380e45fabaafc57b67
Q <42404280+MeasponeS@users.noreply.github.com>
2020-04-03 14:54:42 +0800

    change tab height

--- src/components/CustomHeader.js
+++ src/components/CustomHeader.js
@@ -7,7 +7,7 @@ class CustomHeader extends Component {
     
     render() {
         return (
-            <div style={{width: '100%', height: this.props.height + 'px', display: 'flex', justifyContent: 'flex-start',alignItems: 'center', background:'#f6f6f6', padding: '0 10px', boxSizing: 'border-box'}}>
+            <div style={{width: '100%', height: this.props.height + 'rpx', display: 'flex', justifyContent: 'flex-start',alignItems: 'center', background:'#f6f6f6', padding: '0 10px', boxSizing: 'border-box'}}>
             
             </div>
         );
