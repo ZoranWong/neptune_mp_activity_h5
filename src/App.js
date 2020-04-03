5de70ff4f0575bd6269a9bc7fbb92659841bd1be
Q <42404280+MeasponeS@users.noreply.github.com>
2020-04-03 15:11:28 +0800

    change tab height

--- src/App.js
+++ src/App.js
@@ -128,7 +128,8 @@ class App extends Component {
         return (
             <div className="App" style={{position: 'relative'}}>
                 {/*<CustomHeader title={title} height={this.state.statusHeight} />*/}
-                <div style={{height: this.state.mainHeight + 'px', overflow: 'auto'}}>
+                <div style={{display: 'flex', flexDirection: 'column'}}>
+                {/*<div style={{height: this.state.mainHeight + 'px', overflow: 'auto'}}>*/}
                     {
                         templates.map(template => {
                             return this.renderTemplate(template)
