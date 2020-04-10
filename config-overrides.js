c985caf22560fa7e12f24871a70ed6c428d36dfa
Q <42404280+MeasponeS@users.noreply.github.com>
2020-04-11 02:16:30 +0800

    env

--- config-overrides.js
+++ config-overrides.js
@@ -3,47 +3,11 @@ let WebpackIsomorphicToolsPlugin = require('webpack-isomorphic-tools/plugin');
 // let webpackIsomorphicToolsPlugin = new WebpackIsomorphicToolsPlugin(require('./webpack-isomorphic-tools-configuration')).development();
 const path = require('path');
 const fs = require('fs');
-const DEV_CONFIG = 'src/config/app.dev.js';
-const CONFIG = 'src/config/app.js';
-const PROD_CONFIG = 'src/config/app.prod.js';
 
-function ToolConfigLoader (env) {
-    switch (env) {
-        case 'dev': {
-            loadConfig(DEV_CONFIG, CONFIG);
-            break;
-        }
-        case 'prod': {
-            loadConfig(PROD_CONFIG, CONFIG);
-            break;
-        }
-        default: {
-            loadConfig(DEV_CONFIG, CONFIG);
-            break;
-        }
-    }
-}
-
-function loadConfig (configPath, distConfigPath) {
-    configPath = path.join(__dirname, configPath);
-    distConfigPath = path.join(__dirname, distConfigPath);
-    fs.readFile(configPath, 'utf8', function (error, config) {
-        if (error) {
-            console.log('set front env config fail', error);
-        } else {
-            fs.writeFile(distConfigPath, config, function () {});
-        }
-    });
-}
 
 module.exports = {
     webpack: function(config, env) {
-        // ...add your webpack config
-        // console.log(JSON.stringify(config));
-        // 去掉hash值，解决asset-require-hook资源问题
         console.log('-------------------------------------- +++++++++++++++++++++++++++', env);
-        // ToolConfigLoader(process.env.APP_ENV);
-        console.log(env, process.env.APP_ENV);
         process.APP_ENV = process.env.APP_ENV;
         config.module.rules.forEach(d => {
             d.oneOf &&
--- src/config/app.js
+++ src/config/app.js
@@ -1,15 +1,7 @@
-// const devApiUrl = 'https://www.neptune.kingdomcloud.cn';
-// //const devApiUrl = 'http://neptune.klsfood.cn';
-// //const proApiUrl = 'http://neptune.klsfood.cn'; //正式环境变量,注意修改
-// const proApiUrl = ; //测试服务器,注意修改
-//
-// const nodeDevEnv = process.env.NODE_ENV=='production' ? false : true;
-
-
+import dev from './app.dev'
+import prod from './app.prod'
 
 export default {
-    apiUrl : "https://www.neptune.kingdomcloud.cn",
-    apiPrefix : "",
-    tokenKey:'ACCESS_TOKEN',
-    storageUserKey:'USER_STORAGE',
+    dev,
+    prod
 }
